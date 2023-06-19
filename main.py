6d61b514d4f7156aa53c4e8a587855dbd44dddb7
michalina-nestorowicz <michalina.nestorowicz@capgemini.com>
2023-06-19 13:14:43 +0200

    added one test

--- main.py
+++ main.py
@@ -17,18 +17,19 @@ def main():
     list_countries, path_one, path_two = get_arguments()
     check_file_correct(path_list=[path_one,path_two], format='csv')
 
+    with open (f'./config/df.yml') as cs:
+        df_config = yaml.safe_load(cs)
+    
     with SparkApp('Codac') as spark:
         df_one = DFApp(spark.read(path_one))
         df_two = DFApp(spark.read(path_two))
         
-        column_to_rename_dict = {'cc_t': 'credit_card_type',
-                                 'id': 'client_identifier',
-                                  'btc_a': 'bitcoin_address'}
-        selected_columns_one = ['id', 'email', 'country']
-        selected_columns_two = ['id','btc_a','cc_t']
+        column_to_rename_dict = df_config['column_to_rename_dict']
+        selected_columns_df_one = df_config['selected_columns_df_one']
+        selected_columns_df_two = df_config['selected_columns_df_two']
         columns_to_filter = {'country': list_countries}
-        df_one.select_data(selected_columns_one)
-        df_two.select_data(selected_columns_two)
+        df_one.select_data(selected_columns_df_one)
+        df_two.select_data(selected_columns_df_two)
         df_one.filter_data(columns_to_filter)
         joined_df = DFApp(df_one.df.join(df_two.df, ['id']))
         joined_df.rename_data(column_to_rename_dict)
