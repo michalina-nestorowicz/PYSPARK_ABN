801a04dcbe4df555b9c35b4d8c1ca7786a0fbef2
michalina-nestorowicz <michalina.nestorowicz@capgemini.com>
2023-06-20 07:19:28 +0200

    added filter tests

--- tests/test_df_functions.py
+++ tests/test_df_functions.py
@@ -1,37 +1,36 @@
+import pytest
+import chispa
 from utils.dfapp import DFApp
 from pyspark.sql import SparkSession
 
-spark = (SparkSession.builder.master("local[*]").appName("chispa").getOrCreate())
+
+class TestDFApp:
+    spark = None
+    source_headers = ['test1', 'test2', 'test3']
+    source_data = [['value1', 'value2', 'value3']]
+    renamed_headers = ['New1', 'New2', 'New3']
+
+    @classmethod
+    def setup_class(cls):
+        cls.spark = SparkSession.builder.master("local[*]").appName("chispa").getOrCreate()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.spark.stop()
+
+    @pytest.fixture(name='source_df')
+    def fixture_source_df(cls):
+        return cls.spark.createDataFrame(cls.source_data, cls.source_headers)
+
+    @pytest.fixture(name='renamed_df')
+    def fixture_renamed_df(cls):
+        return cls.spark.createDataFrame(cls.source_data, cls.renamed_headers)
+
+    def test_rename_data(self, source_df, renamed_df):
+        source_df_app = DFApp(source_df)
+        renamed_columns_dict = {self.source_headers[i]: self.renamed_headers[i] for i in range(len(self.source_headers))}
+        source_df_app.rename_data(renamed_columns_dict)
+        chispa.assert_df_equality(renamed_df, source_df_app.df)
+   
 
 
-def test_rename_data():
-    test_headers = ['test1', 'test2', 'test3']
-    df = spark.createDataFrame([('value1', 'value2', 'value3')], test_headers)
-    columns_to_rename = {'test1': 'New1', 'test2': 'New2', 'test3': 'New3'}
-    result = functions.rename_data(df, columns_to_rename)
-    expected_headers = ['New1', 'New2', 'New3']
-    expected = spark.createDataFrame([('value1', 'value2', 'value3')], expected_headers)
-
-    chispa.assert_df_equality(result, expected)
-
-
-def test_filter_data():
-    test_data = [(1, 'Poland', True, 'account_type_1'),
-                 (2, 'England', True, 'account_type_2'),
-                 (3, 'Poland', False, 'account_type_3')]
-    test_headers = ['id', 'country','active', 'account_type']
-    df = spark.createDataFrame(test_data, test_headers)
-    columns_to_filter = {'country': ['Poland', 'France'], 'active': [True]}
-    result = functions.filter_data(df, columns_to_filter)
-    expected = spark.createDataFrame([(1, 'Poland', True, 'account_type_1')], test_headers)
-    chispa.assert_df_equality(result, expected)
-
-
-def test_select_data():
-    test_data = [(1, 'value_test', 'value2', 'value_not_selected')]
-    test_headers = ['id', 'column_test', 'column2', 'column_not_selected']
-    df = spark.createDataFrame(test_data,test_headers)
-    columns_to_select = ['id', 'column2']
-    result = functions.select_data(df, columns_to_select)
-    expected = spark.createDataFrame([(1, 'value2')], columns_to_select)
-    chispa.assert_df_equality(result, expected)
