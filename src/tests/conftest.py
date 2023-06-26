ccff1b457dbb1298336ceb177a6e42f103814a5e
michalina-nestorowicz <michalina.nestorowicz@capgemini.com>
2023-06-26 09:54:38 +0200

    one test file instead of two

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from pyspark.sql import SparkSession
 
-@pytest.fixture(name='spark_session')
-def fixture_spark_session():
-    spark = SparkSession.builder.master("local[*]").appName("chispa").getOrCreate()
+@pytest.fixture(name='spark_session', scope="session")
+def spark_session():
+    spark = SparkSession.builder.master("local[*]").appName("sparktest").getOrCreate()
     yield spark
     spark.stop()
